fae28d8c050eb2ffe484c3379c8a4b6a5f199da9
Scott MacDonald <scott@subscript.ca>
2024-03-04 15:53:42 -0500

    Change contact form to use Form and routeActions

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -1,58 +1,40 @@
-import { component$, $, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import styles from "./contact.module.css";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import { routeAction$, Form, type DocumentHead } from "@builder.io/qwik-city";
 
-interface ContactForm {
-  name: string;
-  email: string;
-  message: string;
-}
+export const useHandleSubmmit = routeAction$(async (formData) => {
+  console.log(formData.name);
+  console.log(formData.email);
+  console.log(formData.message);
+});
 
 export default component$(() => {
-  const formData = useStore<ContactForm>({
-    name: "",
-    email: "",
-    message: "",
-  });
 
-  const handleSubmit$ = $(async () => {
-    console.log(formData.name);
-    console.log(formData.email);
-    console.log(formData.message);
-  });
+  const handleSubmit = useHandleSubmmit();
 
   return (
     <div class={styles.contact}>
       <h1>Contact Us</h1>
-      <form preventdefault:submit onSubmit$={handleSubmit$}>
+      <Form action={handleSubmit}>
         <label>Name</label>
         <input
           type="text"
           name="name"
-          onInput$={(event) =>
-            (formData.name = (event.target as HTMLInputElement).value)
-          }
         />
         <label>Email</label>
         <input
           type="email"
           name="email"
-          onInput$={(event) =>
-            (formData.email = (event.target as HTMLInputElement).value)
-          }
         />
         <label>Message</label>
         <textarea
           name="message"
           class={styles.textArea}
-          onInput$={(event) =>
-            (formData.message = (event.target as HTMLInputElement).value)
-          }
         ></textarea>
         <div class="btn--wrapper">
           <button type="submit">Submit</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 });
